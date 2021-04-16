f75e5a92e98de3d0ae9053c3cfdf457e0aaac882
Raquel Picanço <picanco.raquel@gmail.com>
2021-04-16 08:50:20 -0400

    Implementa informações detalhadas na tela de feedback

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import FeedbackMsg from '../components/FeedbackMsg';
+import FeedbackInfo from '../components/FeedbackInfo';
 
 class Feedback extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class Feedback extends React.Component {
       <div>
         <Header />
         <FeedbackMsg />
+        <FeedbackInfo />
       </div>
     );
   }
