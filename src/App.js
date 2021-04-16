367a48492c3688611d4d812d2cccf89a58a5a8f8
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2021-04-16 19:06:00 -0300

    style(pages/Login): adiciona estilo pagina login

--- src/App.js
+++ src/App.js
@@ -14,14 +14,14 @@ export default function App() {
     <div className="App">
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
-        <Switch>
-          <Route exact path="/" component={ Login } />
-          <Route exact path="/game" component={ Game } />
-          <Route exact path="/settings" component={ Settings } />
-          <Route exact path="/feedback" component={ Feedback } />
-          <Route exact path="/ranking" component={ Ranking } />
-        </Switch>
       </header>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/game" component={ Game } />
+        <Route exact path="/settings" component={ Settings } />
+        <Route exact path="/feedback" component={ Feedback } />
+        <Route exact path="/ranking" component={ Ranking } />
+      </Switch>
     </div>
   );
 }
--- src/components/ButtonSettings.js
+++ src/components/ButtonSettings.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import style from './ButtonSettings.module.css';
 
 class ButtonSettings extends React.Component {
   render() {
     return (
-      <div>
-        <Link to="/settings">
-          <button
-            data-testid="btn-settings"
-            type="button"
-          >
-            Settings
-          </button>
-        </Link>
-      </div>
+      <Link to="/settings">
+        <button
+          className={ style.button }
+          data-testid="btn-settings"
+          type="button"
+        >
+          Settings
+        </button>
+      </Link>
     );
   }
 }
