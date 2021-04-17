8078c63d5815e5cc86bcebee268f3ba920ea6e31
Enio Nicael <enionicael@gmail.com>
2021-04-16 23:48:25 -0300

    Correcoes do eslint.

--- src/components/PlayAgainBtn.jsx
+++ src/components/PlayAgainBtn.jsx
@@ -2,15 +2,8 @@ import React from 'react';
 
 class PlayAgainBtn extends React.Component {
   render() {
-    // xablau
     return (
-      <button
-        type="button"
-        onClick={ this.handleClickBtn }
-        data-testid="btn-play-again"
-      >
-        Jogar novamente
-      </button>
+      <button type="button" data-testid="btn-play-again">Jogar novamente</button>
     );
   }
 }
--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -2,26 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Header from '../components/Header';
-<<<<<<< HEAD
 import PlayAgainBtn from '../components/PlayAgainBtn';
-=======
 import FeedbackMsg from '../components/FeedbackMsg';
 import FeedbackInfo from '../components/FeedbackInfo';
->>>>>>> 66ab3547e1238f365ff60c45705e2aa302ca75cf
 
 class Feedback extends React.Component {
   render() {
     return (
       <div>
         <Header />
-<<<<<<< HEAD
+        <FeedbackMsg />
+        <FeedbackInfo />
         <Link to="/">
           <PlayAgainBtn />
         </Link>
-=======
-        <FeedbackMsg />
-        <FeedbackInfo />
->>>>>>> 66ab3547e1238f365ff60c45705e2aa302ca75cf
       </div>
     );
   }
