69198763d3208120c3850cdffbe4c992cb934096
Nathália Veneziano <36680660+nathaliaveneziano@users.noreply.github.com>
2021-04-19 10:16:47 -0300

    Revert "[Grupo 4] Tela de Jogo"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import Login from '../components/Login';
+import Login from '../components/login';
 
 class Home extends React.Component {
   render() {
     return (
-      <>
-        <Login />
-        <Link to="/settings">
-          <button type="button" data-testid="btn-settings">
-            settings
-          </button>
-        </Link>
-      </>
+      <Login />
     );
   }
 }
