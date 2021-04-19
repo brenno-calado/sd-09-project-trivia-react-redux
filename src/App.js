4226749e7be3474906e93af7e0d19427e2fba261
Nathália <nathaliaveneziano@icloud.com>
2021-04-19 13:15:06 -0300

    add  files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { Feedback, Home, Gaming, Settings, Ranking } from './pages';
+import Home from './pages/Home';
 
 export default function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={ Home } />
-        <Route path="/feedback" component={ Feedback } />
-        <Route path="/gaming" component={ Gaming } />
-        <Route path="/settings" component={ Settings } />
-        <Route path="/ranking" component={ Ranking } />
       </Switch>
     </div>
   );
