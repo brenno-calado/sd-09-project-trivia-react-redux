fb75137306a541e04210f0ea5d4cbe8ed993fdcc
Nathália <nathaliaveneziano@icloud.com>
2021-04-19 13:01:15 -0300

    fix game

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import { Feedback, Home, Gaming, Settings, Ranking } from './pages';
 
 export default function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={ Home } />
+        <Route path="/feedback" component={ Feedback } />
+        <Route path="/gaming" component={ Gaming } />
+        <Route path="/settings" component={ Settings } />
+        <Route path="/ranking" component={ Ranking } />
       </Switch>
     </div>
   );
