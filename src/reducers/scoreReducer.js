2525fe0d1f04bd6d15eafb5b02b9f269a66bcce1
Raquel Picanço <picanco.raquel@gmail.com>
2021-04-19 09:37:30 -0400

    Corrige bug de autalização de score no header

--- src/reducers/scoreReducer.js
+++ src/reducers/scoreReducer.js
@@ -1,4 +1,4 @@
-import { HANDLE_ASSERTION } from '../actions';
+import { HANDLE_ASSERTION, SAVE_USER_DATA } from '../actions';
 
 const INITIAL_STATE = { score: 0, assertions: 0 };
 
@@ -10,6 +10,8 @@ const scoreReducer = (state = INITIAL_STATE, action) => {
       score: state.score + score,
       assertions: state.assertions + 1,
     };
+  case SAVE_USER_DATA:
+    return INITIAL_STATE;
   default:
     return state;
   }
